735c16a7af67f1eb19a9ec8d8e60b89738af8289
Sergio Cardenas <sergio.cardenas@oktana.com>
2022-01-26 13:24:37 -0400

    pass parameters on our apex callsC

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,14 +1,32 @@
 import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.getAllBears() Apex method */
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
+//import getAllBears from '@salesforce/apex/BearController.getAllBears';
+import searchBears from '@salesforce/apex/BearController.searchBears';
 
 export default class BearList extends LightningElement {
-    @wire(getAllBears) bears;
+    searchTerm = ''; //reactive property
+    @wire(searchBears, {searchTerm : '$searchTerm'})
+    bears;
+    //@wire(getAllBears) bears;
     appResources = { // bear static image 
 		bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
 	};
 
+    handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
+	}
     /*
 	//bears; //properties
 	error; // to sjow any error
